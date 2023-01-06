aa8767fc5d75c20d11147d04f8a456d4fd6e3e55
WizardGit <slocumkaiser@gmail.com>
2023-01-05 22:51:41 -0600

    yahtzee upgrade

---
+++ YahtzeeProbabilities/MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace YahtzeeProbabilities
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainLabelOnes = new System.Windows.Forms.Label();
+            this.OnesTextBox = new System.Windows.Forms.TextBox();
+            this.OnesAnswer = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.calbut2 = new System.Windows.Forms.Button();
+            this.templbl = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // MainLabelOnes
+            // 
+            this.MainLabelOnes.AutoSize = true;
+            this.MainLabelOnes.Location = new System.Drawing.Point(101, 97);
+            this.MainLabelOnes.Name = "MainLabelOnes";
+            this.MainLabelOnes.Size = new System.Drawing.Size(84, 13);
+            this.MainLabelOnes.TabIndex = 0;
+            this.MainLabelOnes.Text = "Number of Ones";
+            // 
+            // OnesTextBox
+            // 
+            this.OnesTextBox.Location = new System.Drawing.Point(194, 94);
+            this.OnesTextBox.Name = "OnesTextBox";
+            this.OnesTextBox.Size = new System.Drawing.Size(100, 20);
+            this.OnesTextBox.TabIndex = 1;
+            // 
+            // OnesAnswer
+            // 
+            this.OnesAnswer.AutoSize = true;
+            this.OnesAnswer.Location = new System.Drawing.Point(322, 97);
+            this.OnesAnswer.Name = "OnesAnswer";
+            this.OnesAnswer.Size = new System.Drawing.Size(24, 13);
+            this.OnesAnswer.TabIndex = 2;
+            this.OnesAnswer.Text = "-1%";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(180, 146);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 3;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 219);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // calbut2
+            // 
+            this.calbut2.Location = new System.Drawing.Point(307, 219);
+            this.calbut2.Name = "calbut2";
+            this.calbut2.Size = new System.Drawing.Size(75, 23);
+            this.calbut2.TabIndex = 6;
+            this.calbut2.Text = "Calculate";
+            this.calbut2.UseVisualStyleBackColor = true;
+            this.calbut2.Click += new System.EventHandler(this.calbut2_Click);
+            // 
+            // templbl
+            // 
+            this.templbl.AutoSize = true;
+            this.templbl.Location = new System.Drawing.Point(322, 255);
+            this.templbl.Name = "templbl";
+            this.templbl.Size = new System.Drawing.Size(24, 13);
+            this.templbl.TabIndex = 7;
+            this.templbl.Text = "-1%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "out of";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 368);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.templbl);
+            this.Controls.Add(this.calbut2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.OnesAnswer);
+            this.Controls.Add(this.OnesTextBox);
+            this.Controls.Add(this.MainLabelOnes);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MainLabelOnes;
+        private System.Windows.Forms.TextBox OnesTextBox;
+        private System.Windows.Forms.Label OnesAnswer;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button calbut2;
+        private System.Windows.Forms.Label templbl;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ YahtzeeProbabilities/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YahtzeeProbabilities.Properties;
+
+namespace YahtzeeProbabilities
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }    
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            int p = 0;
+            int.TryParse(OnesTextBox.Text, out p);
+
+            //OnesAnswer.Text = (MultipleRolls(p,6,5,100) * 100).ToString() + " || " + (OneRoll(p, 6) * 100).ToString();
+            //OnesAnswer.Text = NumCombin(4,7).ToString();
+            
+        }
+
+
+        //a sides on dice: 6 good
+        //b number of dice: 5 good
+        //c number of rolls: 1
+        //d number to get: 1 good
+        //e number of d to get: good
+
+        //Probability of getting e of that number in a single roll with 1 roll
+        private float OneRoll(int e, int a = 6, int b=5, int c=1, int d=1)
+        {
+            float prob = 1.0f;
+
+            if (e > b)
+                throw new Exception("You can't get more of a number than dice you are rolling!");
+
+            for (int i = 0; i < e; i++)
+            {
+                prob *=  1.0f / a;
+            }
+            for (int i = e; i < b; i++)
+            {
+                prob *= (a-1.0f) / a;
+            }
+
+            return prob;
+        }
+
+        
+        //Probability of getting e of that number in a single roll with c rolls
+        private float MultipleRolls(int e, int a = 6, int b = 5, int c = 1, int d = 1)
+        {
+            float prob = 1.0f;
+
+            if (e > b)
+                throw new Exception("You can't get more of a number than dice you are rolling!");
+
+            for (int i = 0; i < c; i++)
+            {
+                prob *= (1.0f - OneRoll(e,a,b,c,d));
+            }
+
+            return (1.0f - prob);
+        }
+
+
+        //Number of combinations that "a" dice can be selected with a particular number out of "b" dice
+        // (a out of b)
+        // i.e. how many ways can there be two dice with a number 1 when you have three dice
+        private int NumCombin(int a, int b)
+        {
+            if ((a > b) || (a < 0))
+                throw new Exception("You must request > 0 or < a dice!");
+
+            int topsum = 1;
+            int bottomsum = 1;
+            for (int i = (b - a); i > 0; i--)
+            {
+                topsum *= (b + 1 - i);
+                bottomsum *= i;
+            }
+            return topsum / bottomsum;
+        }
+
+        // Probability of getting "b" of (a number on a six-sided dice) with "a" dice in 1 roll
+        private float Func2(int a, int b)
+        {
+            if ((a > b) || (a < 0))
+                throw new Exception("You must request > 0 or < a dice!");
+
+            float prob = 1.0f;
+
+            for (int i = 0; i < a; i++)
+            {
+                prob *= 1.0f / 6.0f;
+            }
+            for (int i = b; a < i; i--)
+            {
+                prob *= 5.0f / 6.0f;
+            }
+
+            return prob * NumCombin(a,b);
+        }
+
+        // Probability of getting at least "b" of (a number on a six-sided dice) with "a" dice in 1 roll
+        private float AtLeastFunc2(int a, int b)
+        {
+            float prob = 0.0f;
+
+            for (int i = a; i <= b; i++)
+            {
+                prob += Func2(i, b);
+            }
+
+            return prob;
+        }
+
+        // Probability of getting at mostt "b" of (a number on a six-sided dice) with "a" dice in 1 roll
+        private float AtMostFunc2(int a, int b)
+        {
+            float prob = 0.0f;
+
+            for (int i = a; i >= 0; i--)
+            {
+                prob += Func2(i, b);
+            }
+
+            return prob;
+        }
+
+        //2 out of 2 is (1+0)
+        //2 out of 3 is (2+1+0)
+        //2 out of 4 is (3+2+1+0)
+        //2 out of 5 is (4+3+2+1+0)
+
+        //3 out of 3 is (1+0) = 1
+        //3 out of 4 is (2+1+0) + (1+0) = 3+1 = 4 = (4)/(1)
+        //3 out of 5 is (3+2+1+0) + (2+1+0) + (1+0) = 6+3+1=10 = (5*4)/(2*1)
+        //3 out of 6 is (4+3+2+1+0) + (3+2+1+0) + (2+1+0) + (1+0)=10+6+3+1=20= (6*5*4)/(3*2*1)
+
+        //4 out of 4 is (1+0) = 1
+        //4 out of 5 is (2+1+0) + (1+0) + (1+0) = 4 + 1 = 5 = (5)/(1)
+        //4 out of 6 is (3+2+1+0) + (2+1+0) + (1+0) + (2+1+0) + (1+0) + (1+0) = 10 + 4 + 1 = 15 = (6*5)/(2*1)
+        //4 out of 7 is (4+3+2+1+0) + (3+2+1+0) + (2+1+0) + (1+0) + (3+2+1+0) + (2+1+0) + (1+0) + (2+1+0) + (1+0) + (1+0) = 20 + 10 + 4 + 1 = 35 = (7*6*5)/(3*2*1)
+
+
+        //3 out of 5 is (
+        //2 out of 3 is 3
+        //2 out of 3 is 3
+
+        //How many ways can: a be in b
+
+        //(Do recursion for the rolls)
+
+
+        private void calbut2_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+            int.TryParse(textBox1.Text, out a);
+            int.TryParse(textBox2.Text, out b);
+            templbl.Text = NumCombin(a, b).ToString() + " || " + (AtLeastFunc2(a, b)*100).ToString();
+        }
+    }
+}
